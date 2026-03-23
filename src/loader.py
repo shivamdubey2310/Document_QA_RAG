86f7f94d5e1f2afa6a60c2844b4c3cac8837173b
Trishansh Sahane <sahanetrishansh@gmail.com>
2026-03-23 15:27:20 +0530

    modified loader file

--- src/loader.py
+++ src/loader.py
@@ -4,5 +4,4 @@ from langchain_community.document_loaders import PyMuPDFLoader
 def load_files(data_path):
     loader = PyMuPDFLoader(str(data_path))
     documents = loader.load()
-
     return documents 
