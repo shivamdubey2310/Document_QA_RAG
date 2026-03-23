9bc2b050a1af9ea45e327e6ec49ca52e69ffadc8
Trishansh Sahane <sahanetrishansh@gmail.com>
2026-03-23 15:17:49 +0530

    initial commit

---
+++ src/loader.py
@@ -0,0 +1,8 @@
+## import required libraries 
+from langchain_community.document_loaders import PyMuPDFLoader
+
+def load_files(data_path):
+    loader = PyMuPDFLoader(str(data_path))
+    documents = loader.load()
+
+    return documents 
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from loader import load_files
+
+## Step-1 : Loading data 
+docs = load_files("data/Trishansh.pdf")
+print(docs[0])
+
+## Step-2 : Chunking 
